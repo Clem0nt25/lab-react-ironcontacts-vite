81b60b7e82bee8013b71678b6d9a3c48961a12df
Clemens Marckwardt <clemens.marckwardt@gmail.com>
2023-05-09 16:51:09 +0200

    first three

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,89 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import contacts from '../src/contacts.json'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+
+  const addRandomContact = () => {
+    const remainingContacts = contacts.filter(contact => !firstFive.includes(contact));
+
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length);
+    const randomContact = remainingContacts[randomIndex];
+    const updatedContacts = [...firstFive, randomContact];
+    setFirstFive(updatedContacts);
+    console.log(updatedContacts)
+  };
+
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <button onClick={addRandomContact}>Add Random Contact</button>
+
+    <h1>Iron Contacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Oscars?</th>
+            <th>Emmy?</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><img src={firstFive[0].pictureUrl} alt={firstFive[0].name} /></td>
+            <td><p>{firstFive[0].name}</p></td>
+            <td><p>{firstFive[0].popularity}</p></td>
+            <td><p>{firstFive[0].wonOscar ? '🏆' : '-'}</p></td>
+            <td><p>{firstFive[0].wonEmmy ? '🏆' : '-'}</p></td>
+          </tr>
+          <tr>
+            <td><img src={firstFive[1].pictureUrl} alt={firstFive[1].name} /></td>
+            <td><p>{firstFive[1].name}</p></td>
+            <td><p>{firstFive[1].popularity}</p></td>
+            <td><p>{firstFive[1].wonOscar ? '🏆' : '-'}</p></td>
+            <td><p>{firstFive[1].wonEmmy ? '🏆' : '-'}</p></td>
+          </tr>
+          <tr>
+            <td><img src={firstFive[2].pictureUrl} alt={firstFive[2].name} /></td>
+            <td><p>{firstFive[2].name}</p></td>
+            <td><p>{firstFive[2].popularity}</p></td>
+            <td><p>{firstFive[2].wonOscar ? '🏆' : '-'}</p></td>
+            <td><p>{firstFive[2].wonEmmy ? '🏆' : '-'}</p></td>
+          </tr>
+          <tr>
+            <td><img src={firstFive[3].pictureUrl} alt={firstFive[3].name} /></td>
+            <td><p>{firstFive[3].name}</p></td>
+            <td><p>{firstFive[3].popularity}</p></td>
+            <td><p>{firstFive[3].wonOscar ? '🏆' : '-'}</p></td>
+            <td><p>{firstFive[3].wonEmmy ? '🏆' : '-'}</p></td>
+          </tr>
+          <tr>
+            <td><img src={firstFive[4].pictureUrl} alt={firstFive[4].name} /></td>
+            <td><p>{firstFive[4].name}</p></td>
+            <td><p>{firstFive[4].popularity}</p></td>
+            <td><p>{firstFive[4].wonOscar ? '🏆' : '-'}</p></td>
+            <td><p>{firstFive[4].wonEmmy ? '🏆' : '-'}</p></td>
+          </tr>
+        </tbody>
+
+      </table>
+
+
+
+
+
+
+
+
     </div>
   )
 }
